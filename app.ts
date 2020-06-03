3772fa165859809f6dc7bdc897cbc9967ae8a777
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-03 15:08:18 -0300

    Interfaces en Typescript

--- app.ts
+++ app.ts
@@ -1,17 +1,17 @@
 (() => {
-  const retirarDinero = (montoRetirar: number): Promise<number> => {
-    let dineroActual = 1000;
-    return new Promise((resolve, reject) => {
-      if (montoRetirar > dineroActual) {
-        reject("No tenes dinero vieja");
-      } else {
-        dineroActual -= montoRetirar;
-        resolve(dineroActual);
-      }
-    });
+  interface Xmen {
+    nombre: string;
+    edad: number;
+  }
+
+  const enviarMision = (xmen: Xmen) => {
+    console.log(xmen.nombre);
   };
 
-  retirarDinero(1299)
-    .then((mensaje) => console.log(`Te queda ${mensaje}`))
-    .catch((err) => console.warn(err));
+  const xmen: Xmen = {
+    nombre: "dani",
+    edad: 31,
+  };
+
+  enviarMision(xmen);
 })();
