15edefbe8ed5116221ee84c296b75d9d2c9be15f
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-02 22:33:23 -0300

    Creando funcion con parametros
    Funciona con parametros obligatorios, opcionales y opcionales con valor por defecto

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
 (function () {
-  //TEMPLATE STRING
+  //Funcion que tiene 3 parametros: quien es obligatorio, momento opcional y objeto opcional con valor por defecto
+  function activar(
+    quien: string,
+    momento?: string,
+    objeto: string = "batisenal"
+  ) {
+    if (momento) {
+      console.log(`${quien} activo la ${objeto} en ${momento}`);
+    } else {
+      console.log(`${quien} activo la ${objeto}`);
+    }
+  }
 
-  const nombre = "daniel";
-  const apellido = "Velasquez";
-  const edad = 10;
-
-  const templateString = `${nombre} ${apellido} ${edad}`;
+  activar("Loly");
 })();
